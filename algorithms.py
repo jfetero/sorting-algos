fb09a725d883dfae99c5130d1b50fdf590877554
jfetero <jfetero@hawk.iit.edu>
2020-12-31 18:10:29 -0600

    finished GUI working on visualizing algos

--- algorithms.py
+++ algorithms.py
@@ -1,38 +1,34 @@
 import random
 import heapq as h
-
-class Algorithms():
-	
-	def __init__(self, arr):
-		self.arr = arr
+import time
 
 
 
-
-
-
-
-
-def bubble(arr):
+def bubble(arr, drawList, speed):
 	n = len(arr)
 
 	for i in range(1, n):
 		for j in range(n-1):
 			if arr[j] > arr[j+1]:
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				drawList(arr,['red' if x == j or x == j+1 else 'sky blue' for x in range(int(len(arr)))])
+				time.sleep(speed)
 
-	return arr
+	drawList(arr,['SeaGreen3' for _ in range(int(len(arr)))])
 
-def insertion(arr):
+
+def insertion(arr,drawList):
 	for i in range(1, len(arr)):
 		j = i
 		while j > 0 and arr[j-1] > arr[j]:
 			arr[j], arr[j-1] = arr[j-1], arr[j]
 			j -= 1
+			drawList(arr)
+			time.sleep(.2)
 	
 	return arr 
 
-def binary_insertion(arr):
+def binary_insertion(arr, drawList):
 
 	def binary_search(arr, val, left, right):
 		if left == right:
@@ -58,104 +54,148 @@ def binary_insertion(arr):
 		j = binary_search(arr, val, 0, i-1)
 		arr = arr [:j] + [val] + arr[j:i] + arr[i+1:]
 
+	drawList(arr)
+	time.sleep(.2)
+
 	return arr
 
 
-def quick(arr):
+def quick(arr, drawList, speed):
 	def util(arr,low,high): 
 		left = low-1          
-		pivot = arr[high]     
+		pivot = arr[high]    
+
+		drawList(arr, get_color(int(len(arr)), low, high, left, left)) 
+		time.sleep(speed)
 
 		for j in range(low , high):  
 			if arr[j] < pivot:
+				drawList(arr, get_color(int(len(arr)), low, high, left, j, swap = True))
+				time.sleep(speed)
+
 				left += 1 
 				arr[left],arr[j] = arr[j],arr[left] 
+			drawList(arr, get_color(int(len(arr)), low, high, left, j))
+			time.sleep(speed)
 
 		arr[left+1],arr[high] = arr[high],arr[left+1] 
+		drawList(arr, get_color(int(len(arr)), low, high, left, left + 1, swap = True))
+		time.sleep(speed)
 		return left+1
 	  
 	
-	def quick_util(arr,low,high): 
+	def quick_util(arr,low,high, speed): 
 		if low < high: 
 			pivot = util(arr,low,high)  
-			quick_util(arr, low, pivot-1) 
-			quick_util(arr, pivot+1, high)
+			quick_util(arr, low, pivot-1, speed) 
+			quick_util(arr, pivot+1, high, speed)
 
+		drawList(arr,['SeaGreen3' for _ in range(int(len(arr)))]) 
 		return arr
+
+
+	def get_color(length, low, high, wall, idx, swap = False):
+		colors = []
+		for i in range(length):
+			if i >= low and i <= high:
+				colors.append('sky blue')
+			else:
+				colors.append('red')
+
+			if i == high:
+				colors[i] = 'blue'
+			elif i == wall:
+				colors[i] = 'sky blue'
+			elif i == idx:
+				colors[i] = 'yellow2'
+			if swap:
+				if i == wall or i == idx:
+
+					colors[i] = 'SeaGreen3'
+
+		return colors
+
 
 	low = 0
 	high = len(arr)-1
-	return quick_util(arr,low,high)
+	return quick_util(arr,low,high, speed)
 
 
 
-def merge(arr):
+def merge_sort(arr, drawList):
+	if len(arr) <= 1:
+		return arr
 
-	def util(arr):
-		if len(arr) == 1:
-			return arr
+	mid = len(arr)//2
+	left = arr[:mid]
+	right = arr[mid:]
 
-		mid = len(arr)//2
-		left = arr[:mid]
-		right = arr[mid:]
+	left = merge_sort(left, drawList)
+	right = merge_sort(right,drawList)
 
-		left = util(left)
-		right = util(right)
-
-		ret = []
-		i = j = 0
-		
-		while len(left[i:]) > 0 and len(right[j:]) > 0:
-			if left[i] > right[j]:
-				ret.append(right[j])
-				j +=1
-			else:
-				ret.append(left[i])
-				i+=1
-		if len(left) >0:
-			ret.extend(left[i:])
-		if len(right) > 0:
-			ret.extend(right[j:])
-
-		return ret
-
-	return util(arr)
-
-
-def heap(arr):
 	ret = []
-	h.heapify(arr)
-	for _ in range(len(arr)):
-		ret.append(h.heappop(arr))
+	i = j = 0
+	
+	while len(left[i:]) > 0 and len(right[j:]) > 0:
+		if left[i] > right[j]:
+			ret.append(right[j])
+			j +=1
+		else:
+			ret.append(left[i])
+			i+=1
+	if len(left) >0:
+		ret.extend(left[i:])
+	if len(right) > 0:
+		ret.extend(right[j:])
+
+	drawList(ret)
+	time.sleep(.2)
 
 	return ret
 
 
-def radix(arr):
+def heap(arr, drawList):
+	ret = []
+	h.heapify(arr)
+	for _ in range(len(arr)):
+		ret.append(h.heappop(arr))
+	drawList(ret)
+	time.sleep(.2)
+
+	return ret
+
+
+def radix(arr, drawList):
 	num = len(str(max(arr)))
 
 	for digit in range(num):
 		bucket = [[] for i in range(10)]
 		for x in arr:
 			idx = x // 10 ** digit % 10
-			bucket[idx].append(x)
+			bucket[int(idx)].append(x)
+			drawList(arr)
+			time.sleep(.2)
 		arr = []
 		for t in bucket:
 			arr.extend(t)
+
+
 	return arr
 
-def counting(arr):
+def counting(arr, drawList):
 	mx = max(arr)	
-	temp = [0 for _ in range(mx+1)]
+	temp = [0 for _ in range(int(mx+1))]
 	ret = []
 
 	for x in arr:
-		temp[x] +=1
+		temp[int(x)] +=1
 
 	for i in range(len(temp)):
-		if temp[i] != 0:
-			ret += [i for _ in range(temp[i])]
+		if temp[int(i)] != 0:
+			ret += [i for _ in range(int(temp[i]))]
 
+	drawList(ret)
+	time.sleep(.2)
 	return ret
 
  
@@ -169,12 +209,12 @@ def counting(arr):
 
 	
 
+if __name__ == '__main__':
+	#arr = [random.randint(0,50_000) for x in range(10_000)]
+	#arr1 = [random.randint(0,10000) for x in range(10_000)]
+	data = [9,8,11,7,6,5,10,4,3,2,12,1]
 
-#arr = [random.randint(0,10_00) for x in range(10_000_000)]
-arr1 = [random.randint(0,10000) for x in range(10_000)]
-# data = [9,8,11,7,6,5,10,4,3,2,12,1]
-
-# print(arr)
-print(radix(arr1))
+	print(data)
+	print(radix(data))
 
 
---
+++ sorting_algos_gui.py
@@ -0,0 +1,123 @@
+from tkinter import *
+from tkinter import ttk
+import numpy as np
+import algorithms
+from random import shuffle as shuff
+import time
+
+
+#========== Window ==============
+wind = Tk()
+wind.title('Sorting Algorithms')
+wind.maxsize(1000,750)
+wind.config(bg = 'gainsboro')
+
+#========= Variables ==============
+selected_algo = StringVar()
+algos = ['Bubble Sort', 'Insertion Sort', 'Binary Insertion Sort', 'Quick Sort', 'Merge Sort', 'Heap Sort', 'Radix Sort', 'Counting Sort']
+arr = []
+
+#========= Functions ===========
+def generate():
+	# print(f'Selected Algorthm: {selected_algo.get()}')
+	global arr
+	try:
+		size = int(size_input.get()) if int(size_input.get()) <= 400 else 400
+	except:
+		size = 20
+
+	arr = list(np.linspace(1,size, size))
+	shuff(arr)
+	drawList(arr, ['sky blue' for _ in range(int(len(arr)))])
+	
+
+
+def drawList(arr, colors):
+	canv.delete('all')
+
+	height = 530
+	width = 1000
+	b_width = width / (len(arr)+.5)
+	pad = 5
+	space = 1
+	n_arr = [i / max(arr) for i in arr]
+
+	for i, h in enumerate(n_arr):
+		x0 = i * b_width + pad + space
+		y0 = height - h * 515
+
+		x1 = (i + 1) * b_width+space
+		y1 = height
+
+		canv.create_rectangle(x0,y0,x1,y1, fill = colors[i])
+		#canv.create_text((x0+x1)//2 -2, y0, anchor = SW, text = str(arr[i]))
+
+	wind.update()
+
+
+def start():
+	global arr
+	s = speed.get()
+	print(selected_algo.get())
+	if selected_algo.get() == algos[0]:
+		algorithms.bubble(arr, drawList, s)
+
+	if selected_algo.get() == algos[1]:
+		algorithms.insertion(arr, drawList, s)
+
+	if selected_algo.get() == algos[2]:
+		algorithms.binary_insertion(arr, drawList, s)
+
+	if selected_algo.get() == algos[3]:
+		algorithms.quick(arr, drawList, s)
+
+	if selected_algo.get() == algos[4]:
+		algorithms.merge_sort(arr, drawList, s)
+
+	if selected_algo.get() == algos[5]:
+		algorithms.heap(arr, drawList, s)
+
+	if selected_algo.get() == algos[6]:
+		algorithms.radix(arr, drawList, s)
+
+	if selected_algo.get() == algos[7]:
+		algorithms.counting(arr, drawList, s)
+
+
+
+
+#====================== User Interface =========================
+frame = Frame(wind, width = 1000, height = 200, bg='grey')
+frame.grid(row = 0, column = 0, pady = 5)
+
+algo_label = Label(frame, text = 'Algorithm: ', bg = 'grey')
+algo_label.grid(row=0, column=0, padx = 5, pady = 5, sticky= W)
+
+algo_menu = ttk.Combobox(frame, textvariable = selected_algo, values = algos)
+algo_menu.grid(row = 0, column = 1, padx = 5, pady = 5)
+algo_menu.current(0)
+
+size_label = Label(frame, text = 'Size: ', bg = 'grey')
+size_label.grid(row=1, column=0, padx = 5, pady = 5, sticky= W)
+
+size_input = Entry(frame)
+size_input.grid(row=1, column=1, padx = 5, pady = 5, sticky= W)
+
+speed = Scale(frame, from_=0.01, to= .2, length = 200, digits = 2, resolution = .001, orient = HORIZONTAL, label = 'Speed (s): ')
+speed.grid(row = 0, column = 3, padx = 5, pady = 5)
+
+# buttons
+gen_list_button = Button(frame, text = 'Generate List', command = generate, bg = 'blue')
+gen_list_button.grid(row = 1, column = 2, padx = 5, pady = 5)
+
+start_button = Button(frame, text = 'Start Sort', command = start, bg = 'orange')
+start_button.grid(row = 0, column = 2, padx = 5, pady = 5)
+
+ 
+
+
+#=================================================================
+canv = Canvas(wind, width = 1000, height = 530, bg = 'snow')
+canv.grid(row = 1, column = 0 ,pady = 5)
+
+wind.mainloop()
